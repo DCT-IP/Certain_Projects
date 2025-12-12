4b9f0683440b5f75f1c9c6be50d01e96ede975eb
Dheeman Chaitanya Thakur <dheemanchaitanyathakur@gmail.com>
2025-12-13 01:44:55 +0530

    Enhance comments for clarity on bit operations

--- C_Stuff/Pointers_Mem/BitOps.c
+++ C_Stuff/Pointers_Mem/BitOps.c
@@ -1,10 +1,13 @@
-/*In this I will try to do 
-    1.Basic bitwise operations
-    2.Bit Manipulation
-    3.Masks
-    4.Special Bit Tricks
+/*
+In this I will try to do 
+    1.Basic bitwise operations - and, or , xor, left shift, right shift
+    2.Bit Manipulation - checking a bit to be 1, setting a bit to 1, clearing a bit(set to 0), toggle a bit, LSB(extract last), check even
+    3.Masks - mask to:- 1.extract specific bits,
+                        2.to turn on/off specific bits,
+                        3.dynamic masks using shifts.
+    4.Special Bit Tricks - swapping two numbers,
     5.2's Compliment
-    6.Shifting details*/
+    */
 //The main focus is on understanding it so the project for this section can be done easily
 
 //TBD later
